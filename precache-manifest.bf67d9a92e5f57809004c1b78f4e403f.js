4addcb711ef536501eebfbafc76f959deba72e6c
Asad <chaudryasad97@gmail.com>
2019-07-22 21:22:53 -0400

    Updates

---
+++ precache-manifest.bf67d9a92e5f57809004c1b78f4e403f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f57a182f9cd6d4d1ffec6f1b7f6b10e",
+    "url": "/eventmanager/index.html"
+  },
+  {
+    "revision": "fd607553301338488cdd",
+    "url": "/eventmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "070bba769f3ee69036fc",
+    "url": "/eventmanager/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "fd607553301338488cdd",
+    "url": "/eventmanager/static/js/2.71ddfead.chunk.js"
+  },
+  {
+    "revision": "070bba769f3ee69036fc",
+    "url": "/eventmanager/static/js/main.6da257f8.chunk.js"
+  },
+  {
+    "revision": "1c8287c090aac17864a3",
+    "url": "/eventmanager/static/js/runtime~main.7f8e44f8.js"
+  }
+]);
